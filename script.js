b46062b1809732f8023da7094c5a0f4a9816582c
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-03-04 17:45:15 +0100

    boucle for while

---
+++ script.js
@@ -0,0 +1,34 @@
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = [
+  "Pas de panique !",
+  "La vie, l'univers et le reste",
+  "Merci pour le poisson",
+];
+
+let score = 0;
+
+let choix = prompt("Veillez choisir la Liste : mots ou phrases");
+while (choix !== "mots" && choix !== "phrases") {
+  choix = prompt("Veillez choisir la Liste : mots ou phrases");
+}
+
+if (choix === "mots") {
+  for (let i = 0; i < listeMots.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i]);
+    if (motUtilisateur === listeMots[i]) {
+      score++;
+    }
+  }
+  console.log("Votre score est de " + score + " sur "  + listeMots.length );
+} else {
+  for (let i = 0; i < listePhrases.length; i++) {
+    let motUtilisateur = prompt("Entrez la phrase : " + listePhrases[i]);
+    if (motUtilisateur === listePhrases[i]) {
+      score++;
+    }
+  }
+  console.log("Votre score est de " + score + " sur "  + listePhrases.length );
+}
+
+
