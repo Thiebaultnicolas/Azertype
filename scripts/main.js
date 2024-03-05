0c0ef5fae40088c7122fef8fac38e4df9d59d59b
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-03-05 16:46:35 +0100

    Récupération d'élements depuis main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-lancerJeu();
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let divZoneProposition = document.querySelector(".zoneProposition")
+console.log(divZoneProposition)
+
+let spanScore = document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)
